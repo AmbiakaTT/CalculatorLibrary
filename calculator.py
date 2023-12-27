7f5156871023a1125d4637f4dfd369d3ef5e83a0
Rainn <56089814+AmbiakaTT@users.noreply.github.com>
2023-12-27 14:42:25 +0700

    setup

--- calculator.py
+++ calculator.py
@@ -1,11 +1,14 @@
 def add(first_term, second_term):
     return first_term + second_term
     
+
 def subtract(first_term, second_term):
     return first_term - second_term
 
+
 def multiply(first_term, second_term):
     return first_term * second_term
+
 
 def divide(first_term, second_term):
     if second_term == 0:
